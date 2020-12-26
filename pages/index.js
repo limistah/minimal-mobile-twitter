e5bad06a6361938364725a3a8268da6a424dabc3
Aleem Isiaka <aleemisiaka@gmail.com>
2020-12-26 06:14:05 +0100

    Improvement

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
-import Head from "next/head";
-
 export default function Home() {
   return (
     <>
-      <div class="container mx-auto flex flex-col h-full relative">
+      <div className="container mx-auto flex flex-col h-full relative">
         <div className="w-100 h-14 bg-purple-300">Top Bar</div>
         <div className="w-100 flex-1 bg-red-400">Content Container</div>
-        <div className="w-100 h-14 bg-green-500">Footer with Nav buttons</div>
+        <div className="w-100 h-14 bg-green-500">Nav Bar</div>
       </div>
     </>
   );
