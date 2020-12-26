ce8bcfc2a54b3c207f8786312232d60b89ee13dc
Aleem Isiaka <aleemisiaka@gmail.com>
2020-12-26 08:19:03 +0100

    Top Bar

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
+import TopBar from "./components/TopBar";
+
 export default function Home() {
   return (
     <>
       <div className="container mx-auto flex flex-col h-full relative">
-        <div className="w-100 h-14 bg-purple-300">Top Bar</div>
+        <div className="w-100 h-14 bg-purple-50">
+          <TopBar />
+        </div>
         <div className="w-100 flex-1 bg-red-400">Content Container</div>
         <div className="w-100 h-14 bg-green-500">Nav Bar</div>
       </div>
